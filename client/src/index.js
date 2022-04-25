e43a95d438047fa278b073bc4bbdee03a44e527e
BolBol <Bilel.bouneb90@gmail.com>
2022-04-25 19:01:35 +0100

    Install redux and Create store

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,33 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
+>>>>>>> Stashed changes
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+<<<<<<< Updated upstream
   <React.StrictMode>
     <App />
   </React.StrictMode>
+=======
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+>>>>>>> Stashed changes
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  userInfo: {},
+  token: null,
+  errors: null,
+  isAuth: false,
+};
+const userReducer = (state = initState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
+export default userReducer;
