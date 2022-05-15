15a0a6c6f5342f9b7da88b81176ac1d8fb5efd9d
Amani0905 <ameni-bouzouita@hotmail.com>
2022-05-15 17:56:17 +0200

    Navbar/responsive

--- client/src/Components/Application.js
+++ client/src/Components/Application.js
@@ -8,7 +8,7 @@ const Application = () => {
     <h2 style={{padding:"20px",textAlign:"center",fontWeight:900,fontSize:'35px'}}>Secteurs cibles ayant des compétences et un potentiel non adressé</h2>
     <div className="square bg img1">
     <div className="content-2">
-    <div className="table">
+    <div className="table-1">
         <div className="table-cell">
         Biotechnologies<br/>
         Santé<br/>
@@ -19,7 +19,7 @@ const Application = () => {
   </div>
   <div className="square bg img2">
     <div className="content-2">
-      <div className="table">
+      <div className="table-1">
         <div className="table-cell">
         Energie renouvelables<br/>
 Environnement<br/>
